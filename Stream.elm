679ae8e4bf60bd1428252a370c1c54ce8efcf65e
Max New <maxsnew@gmail.com>
2013-08-27 21:06:55 -0700

    Some stream utilites

--- Stream.elm
+++ Stream.elm
@@ -31,3 +31,36 @@ foldr f init xs' = let go xs = case xs of
                          End        -> lazyPure init
                          More x txs -> lazyMap (f x) (lazyBind txs go)
                    in go xs'
+
+-- toList might not terminate!
+toList : Stream a -> [a]
+toList xs = case xs of
+  End        -> []
+  More x txs -> x :: (toList . runLazy <| txs)
+
+fromList : [a] -> Stream a
+fromList xs' = case xs' of
+  [] -> End
+  x :: xs -> x :~: (lazyPure . fromList <| xs) 
+
+-- Lazy Zip
+zip : Stream a -> Stream b -> Stream (a, b)
+zip xs ys = case xs of
+  End -> End
+  More x xs -> case ys of
+    End -> End
+    More y ys -> (x, y) :~: ((zip `lazyMap` xs) `lazyAp` ys)
+
+zipWith : (a -> b -> c) -> Stream a -> Stream b -> Stream c
+zipWith f xs ys = map (uncurry f) <| zip xs ys
+
+-- Things you can't do with lists
+repeat : a -> Stream a
+repeat x = x :~: (\() -> repeat x)
+
+cycle : [a] -> Stream a
+cycle xs = fromList xs +~+ cycle xs
+
+iterate : (a -> a) -> a -> Stream a
+iterate f x = let y = f x 
+              in y :~: (\() -> iterate f y)
