df4fa398014b1b9c986bdba2d6b679a9a2898216
Max New <maxsnew@gmail.com>
2013-10-24 16:11:19 -0500

    Add faster fib

--- Main.elm
+++ Main.elm
@@ -12,5 +12,15 @@ nats = 0 :~: (\() -> LList.map ((+) 1) nats)
 fibs : LList Int
 fibs = 0 :~: Lazy.pure (1 :~: (\() -> LList.zipWith (+) fibs (LList.tail fibs)))
 
+-- Much faster, but is it because it's avoiding recomputation or just making fewer function calls?
+fibs_iter' : Int -> Int -> LList Int
+fibs_iter' n m = n :~: (\() -> (fibs_iter' m (n + m)))
+
+fibs' : LList Int
+fibs' = fibs_iter' 0 1
+
 -- Prohibitively slow for large number of values taken.
 main = asText . LList.take 25 <| fibs
+
+-- This is way faster
+-- main = asText . LList.take 25 <| fibs'
