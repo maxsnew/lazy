5230f7abbe0520968c55a30bfaeeb1a505d6e7e8
Max New <maxsnew@gmail.com>
2013-12-20 01:02:52 -0600

    Update Example description

--- Main.elm
+++ Main.elm
@@ -8,19 +8,35 @@ nats : Stream Int
 nats = S.iterate (\n -> n + 1) 0
 
 -- Just like you'd write it in Haskell
--- There are ways to write it that are faster, but with memoizing thunks you don't need to
+
+{- Without memoized lazy values this is equivalent to the naive
+   recursive version:
+
+```Haskell
+fib 0 = 0
+fib 1 = 1
+fib n = (fib n) + (fib (n - 1))
+```
+-}
 fibs : Stream Int
 fibs =
   S.cons 0 <| \() ->
   S.cons 1 <| \() ->
   S.zipWith (+) fibs (S.tail fibs)
 
--- Much faster, but is it because it's avoiding recomputation or just making fewer function calls?
+{- Equivalent to this smarter version of fib:
+```Haskell
+fib = fib_help 0 1
+
+fib_help m n 0 = m
+fib_help m n k = fib_help n (m + n) (k - 1)
+```
+-}
 fibs' : Stream Int
 fibs' = S.unfold (\(m, n) -> (m, (n, m + n))) (0, 1)
 
--- Prohibitively slow for large number of values taken.
+-- Fast!
 main = asText . S.take 100 <| fibs'
 
--- This is way faster
--- main = asText . S.take 25 <| fibs'
+-- Ungodly slow!
+-- main = asText . S.take 25 <| fibs
