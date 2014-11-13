bb8bb071fe216fe88e1284989e97b980a1b48e19
Max New <maxsnew@gmail.com>
2014-11-13 07:19:30 -0500

    Update elm_deps and Main now that memoization is implemented

--- Main.elm
+++ Main.elm
@@ -8,14 +8,13 @@ nats : Stream Int
 nats = S.iterate (\n -> n + 1) 0
 
 -- Just like you'd write it in Haskell
+{- This is equivalent to a memoized version of the following function:
 
-{- Without memoized lazy values this is equivalent to the naive
-   recursive version:
+```haskell
+fib = fib_help 0 1
 
-```Haskell
-fib 0 = 0
-fib 1 = 1
-fib n = (fib n) + (fib (n - 1))
+fib_help m n 0 = m
+fib_help m n k = fib_help n (m + n) (k - 1)
 ```
 -}
 fibs : Stream Int
@@ -24,17 +23,5 @@ fibs =
   S.cons 1 <| \() ->
   S.zipWith (+) fibs (S.tail fibs)
 
-{- Equivalent to this smarter version of fib:
-```Haskell
-fib = fib_help 0 1
-
-fib_help m n 0 = m
-fib_help m n k = fib_help n (m + n) (k - 1)
-```
--}
-fibs' : Stream Int
-fibs' = S.unfold (\(m, n) -> (m, (n, m + n))) (0, 1)
-
--- Both fast!
--- main = asText . S.take 100 <| fibs'
-main = asText << S.take 25 <| fibs
+-- fast!
+main = asText << S.take 1000 <| fibs
